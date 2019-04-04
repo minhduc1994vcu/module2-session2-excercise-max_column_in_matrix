6a71db786240959f0d9d3f57917e252f1f8325dc
minhduc <minhduc1994vcu@gmail.com>
2019-04-05 00:03:07 +0700

    find max sum's column of unknown matrix

--- src/main/java/sumColumn.java
+++ src/main/java/sumColumn.java
@@ -1,23 +1,57 @@
-public class sumColumn {
-    public static void main(String[] args) {
-        int[][] matrix = {
-                {4, 5, 6},
-                {5, 9, 8},
-                {6, 1, 2}
-        };
-        int[] sumColumn = new int[3];
-        for (int j = 0; j < matrix[0].length; j++) {
 
-            for (int i = 0; i < matrix.length; i++) {
-                sumColumn[j] += matrix[i][j];
+import java.util.Scanner;
+
+public class sumColumn {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter row of matrix: ");
+        int row = scanner.nextInt();
+        System.out.print("enter column of matrix: ");
+        int column = scanner.nextInt();
+
+        int[][] matrix = new int[row][column];
+
+        inputMatrix(matrix, row, column);
+
+        showMatrix(matrix, row, column);
+
+       int[] sumColumn = new int[column];
+        for (int columnIndex = 0; columnIndex < matrix[0].length; columnIndex++) {
+
+            for (int[] ints : matrix) {
+                sumColumn[columnIndex] += ints[columnIndex];
             }
         }
         int maxSumColumn = sumColumn[0];
-        for (int k = 1; k < sumColumn.length; k++) {
+        int indexOfMaxSumColumn = 0;
+        for (int k = 0; k < sumColumn.length; k++) {
             if (sumColumn[k] > maxSumColumn) {
                 maxSumColumn = sumColumn[k];
+                indexOfMaxSumColumn = k;
             }
         }
-        System.out.println(maxSumColumn);
+        System.out.println(indexOfMaxSumColumn + " is max which sum is: " + maxSumColumn);
+
+
+    }
+
+    private static void inputMatrix(int[][] matrix, int row, int column) {
+        Scanner scanner = new Scanner(System.in);
+        for (int rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < matrix[rowIndex].length; columnIndex++) {
+                System.out.print("element at row " + rowIndex + " and column " + columnIndex + " is: ");
+                 matrix[rowIndex][columnIndex] = scanner.nextInt();
+            }
+        }
+    }
+
+    private static void showMatrix(int[][] matrix, int row, int column) {
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                System.out.print(anInt + "\t");
+            }
+            System.out.println();
+        }
     }
 }
